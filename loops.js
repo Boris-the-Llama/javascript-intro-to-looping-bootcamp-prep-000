a4920376151fc6831c4f872a3212221f18b13569
Dan Brett <boristhellama99@gmail.com>
2018-01-11 18:52:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
 function forLoop(array) {
   for (var i = 0; i < 26; i++) {
     if (i == 1) {
-      array.push("I am 1 strange loop")
+      array.push("I am 1 strange loop.");
     }
+  } else {
+    
   }
 }
